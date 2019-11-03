8951135839c499cd49743c9c754da9707db689c8
rainyuxuan <lyx2001htw@126.com>
2019-11-03 16:52:38 -0500

    loginPage updated

--- screens/login/LoginPage.js
+++ screens/login/LoginPage.js
@@ -1,24 +1,71 @@
 import React from "react";
-import { Text, View, Button } from "react-native";
+import { Text, View, Button, StyleSheet, TextInput } from "react-native";
+
 
 class LoginPage extends React.Component {
+
+    state = {
+      isLoggedIn: false,
+      isCorrectLogin: false,
+      emailPrefix: '',
+      password: '',
+      message: ''
+    }
+
     render() {
         return (
-            <View
-                style={{
-                    flex: 1,
-                    alignItems: "center",
-                    justifyContent: "center"
-                }}
-            >
-                <Text style={{ fontSize: 30 }}>Linkus login page</Text>
+            <View style={styles.loginBackground}>
+              <View style={styles.iconSection}>
+                /* There should be an icon image*/
+              </View>
+              <View style={styles.inputBar}>
+                <View style={{flex:2}}>
+                 <TextInput placeholder='Email' />
+                </View>
+                <View style={{flex:1, alignItems: 'center', justifyContent: 'flex-start'}}>
+                  <Text>@mail.utoronto.ca</Text>
+                </View>
+
+              </View>
+              <View>
+                 <TextInput placeholder='Password' />
+              </View>
+              <View>
+                /* Button */
                 <Button
-                    onPress={() => this.props.navigation.navigate("Home")}
-                    title="Login"
+                  onPress{() =>
+                    this.props.navigation.navigate("Home")
+                  }
+                  title="Login"
                 />
+
+              </View>
+              <View styles={{flex:2}}>
+              </View>
             </View>
         );
     }
 }
 
 export default LoginPage;
+
+
+
+const styles = StyleSheet.create({
+  loginBackground:{
+    flex: 1,
+    flexDirection: 'column',
+
+  },
+  iconSection:{
+    flex: 2,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'white'
+  },
+  inputBar: {
+    flex: 1,
+    flexDirection: 'row'
+  },
+
+});
