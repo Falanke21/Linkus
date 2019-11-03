068bdf4f757839622891fe3bcc5556f25195394e
Falanke21 <1065314944@qq.com>
2019-11-03 14:48:38 -0500

    factored pages into folders

--- App.js
+++ App.js
@@ -4,10 +4,10 @@ import { createStackNavigator } from "react-navigation-stack";
 import { createBottomTabNavigator } from "react-navigation-tabs";
 import { Ionicons } from "@expo/vector-icons";
 
-import LoginPage from "./LoginPage";
-import HistoryPage from "./HistoryPage";
-import RecordPage from "./RecordPage";
-import ProfilePage from "./ProfilePage"
+import LoginPage from "./screens/login/LoginPage";
+import HistoryPage from "./screens/home/HomePage";
+import RecordPage from "./screens/forum/ForumPage";
+import ProfilePage from "./screens/profile/ProfilePage"
 
 const getTabBarIcon = (navigation, focused, tintColor) => {
     const { routeName } = navigation.state;
