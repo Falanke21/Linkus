b5758f3cb87c2e8ce359833a39a4f492d75009e1
kexinlin <kexin.lin@mail.utoronto.ca>
2019-11-04 14:48:36 -0500

    fix: add package react-native-vector-icons and fix bug of text rendering

--- screens/home/HomePage.js
+++ screens/home/HomePage.js
@@ -2,19 +2,19 @@ import React from "react";
 import { Text, View } from "react-native";
 
 class HomePage extends React.Component {
-    render() {
-        return (
-            <View
-                style={{
-                    flex: 1,
-                    justifyContent: "center",
-                    alignItems: "center"
-                }}
-            >
-                <Text>Home page</Text>
-            </View>
-        );
-    }
+  render() {
+    return (
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center"
+        }}
+      >
+        <Text>Home page</Text>
+      </View>
+    );
+  }
 }
 
 export default HomePage;
