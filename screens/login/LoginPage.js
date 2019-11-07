12117071a2871e5b627154f261e8adb7a05244d1
rainyuxuan <lyx2001htw@126.com>
2019-11-06 19:25:33 -0500

    Update LoginPage.js

--- screens/login/LoginPage.js
+++ screens/login/LoginPage.js
@@ -1,71 +1,63 @@
 import React from "react";
 import { Text, View, Button, StyleSheet, TextInput } from "react-native";
 
-
 class LoginPage extends React.Component {
+  state = {
+    isLoggedIn: false,
+    isCorrectLogin: false,
+    emailPrefix: "",
+    password: "",
+    message: ""
+  };
 
-    state = {
-      isLoggedIn: false,
-      isCorrectLogin: false,
-      emailPrefix: '',
-      password: '',
-      message: ''
-    }
-
-    render() {
-        return (
-            <View style={styles.loginBackground}>
-              <View style={styles.iconSection}>
-                /* There should be an icon image*/
-              </View>
-              <View style={styles.inputBar}>
-                <View style={{flex:2}}>
-                 <TextInput placeholder='Email' />
-                </View>
-                <View style={{flex:1, alignItems: 'center', justifyContent: 'flex-start'}}>
-                  <Text>@mail.utoronto.ca</Text>
-                </View>
-
-              </View>
-              <View>
-                 <TextInput placeholder='Password' />
-              </View>
-              <View>
-                /* Button */
-                <Button
-                  onPress{() =>
-                    this.props.navigation.navigate("Home")
-                  }
-                  title="Login"
-                />
-
-              </View>
-              <View styles={{flex:2}}>
-              </View>
-            </View>
-        );
-    }
+  render() {
+    return (
+      <View style={styles.loginBackground}>
+        <View style={styles.iconSection}></View>
+        <View style={styles.inputBar}>
+          <View style={{ flex: 2 }}>
+            <TextInput placeholder="Email" />
+          </View>
+          <View
+            style={{
+              flex: 1,
+              alignItems: "center",
+              justifyContent: "flex-start"
+            }}
+          >
+            <Text>@mail.utoronto.ca</Text>
+          </View>
+        </View>
+        <View>
+          <TextInput placeholder="Password" />
+        </View>
+        <View>
+          <Button
+            onPress={() => this.props.navigation.navigate("Home")}
+            title="Login"
+          />
+        </View>
+        <View styles={{ flex: 2 }}></View>
+      </View>
+    );
+  }
 }
 
 export default LoginPage;
 
-
-
 const styles = StyleSheet.create({
-  loginBackground:{
+  loginBackground: {
     flex: 1,
-    flexDirection: 'column',
-
+    flexDirection: "column"
   },
-  iconSection:{
+  iconSection: {
     flex: 2,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: 'white'
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "white"
   },
   inputBar: {
     flex: 1,
-    flexDirection: 'row'
-  },
-
+    flexDirection: "row"
+  }
 });
