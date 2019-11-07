24b8a21056e26145179256bdbd22260477c64a94
rainyuxuan <lyx2001htw@126.com>
2019-11-06 20:27:31 -0500

    New layout for LoginPage!

--- screens/login/LoginPage.js
+++ screens/login/LoginPage.js
@@ -15,29 +15,66 @@ class LoginPage extends React.Component {
       <View style={styles.loginBackground}>
         <View style={styles.iconSection}></View>
         <View style={styles.inputBar}>
-          <View style={{ flex: 2 }}>
-            <TextInput placeholder="Email" />
+          <View style={{flex:1}}></View>
+          <View
+            style={{
+              flex: 2,
+              alignItems: 'flex-end',
+              justifyContent: 'center',
+              /*borderWidth: 1*/
+            }}>
+            <TextInput
+              placeholder="UT Email"
+              textContentType="username"
+              style={{
+                borderColor:"black",
+                borderWidth:1,
+                borderRadius:5,
+                textAlign:'center',
+                width: 120
+                }}
+              />
           </View>
           <View
             style={{
-              flex: 1,
-              alignItems: "center",
-              justifyContent: "flex-start"
-            }}
-          >
-            <Text>@mail.utoronto.ca</Text>
+              flex: 2,
+              alignItems: "flex-start",
+              justifyContent: "center",
+            }}>
+            <Text> @mail.utoronto.ca</Text>
+
           </View>
+          <View style={{flex:1}}></View>
         </View>
-        <View>
-          <TextInput placeholder="Password" />
+        <View style={styles.inputBar}>
+          <TextInput
+            placeholder="Password"
+            textContentType="password"
+            secureTextEntry={true}
+            style={{
+              borderColor:"black",
+              borderWidth:1,
+              borderRadius:5,
+              textAlign:'center'
+              }}
+
+          />
         </View>
-        <View>
+        <View style={{
+                flex: 1,
+                borderWidth: 0,
+                borderColor: "black",
+                justifyContent:'center',
+                alignItems: 'center'
+        }}>
           <Button
             onPress={() => this.props.navigation.navigate("Home")}
             title="Login"
           />
         </View>
-        <View styles={{ flex: 2 }}></View>
+        <View style={styles.iconSection}>
+          <Text> 怎么固定textInput的长度？ </Text>
+        </View>
       </View>
     );
   }
@@ -51,13 +88,17 @@ const styles = StyleSheet.create({
     flexDirection: "column"
   },
   iconSection: {
-    flex: 2,
+    flex: 8,
     alignItems: "center",
     justifyContent: "center",
-    backgroundColor: "white"
+    backgroundColor: "rgb(138, 193, 230)"
   },
   inputBar: {
     flex: 1,
-    flexDirection: "row"
+    flexDirection: "row",
+    borderWidth: 0,
+    borderColor: "black",
+    justifyContent:'center',
+    alignItems: 'center'
   }
 });
