cdb38af0cbdfbdf1b3c98b54031898eb09e2fa2d
kexinlin <kexin.lin@mail.utoronto.ca>
2019-11-10 15:45:33 -0500

    remove material design

--- screens/link/LinkusPage.js
+++ screens/link/LinkusPage.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Text, View } from "react-native";
-import {SearchBar} from 'react-native-elements';
+import { SearchBar } from "react-native-elements";
 class LinkusPage extends React.Component {
   state = {
     search: ""
