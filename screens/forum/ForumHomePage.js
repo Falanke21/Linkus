cfba0dfc2db6e53fc0bcddac0675ba014bdc9d61
kexinlin <kexin.lin@mail.utoronto.ca>
2019-11-10 15:33:14 -0500

    add material design

---
+++ screens/forum/ForumHomePage.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import { Container, Header, Tab, Tabs, ScrollableTab } from "native-base";
+import ChatTab from "./ChatTab";
+import StudyTab from "./StudyTab";
+export default class TabsScrollableExample extends Component {
+  render() {
+    return (
+      <Container>
+        <Header hasTabs />
+        <Tabs renderTabBar={() => <ScrollableTab />}>
+          <Tab heading="聊天闲谈">
+            <ChatTab />
+          </Tab>
+          <Tab heading="学术讨论">
+            <StudyTab />
+          </Tab>
+        </Tabs>
+      </Container>
+    );
+  }
+}
