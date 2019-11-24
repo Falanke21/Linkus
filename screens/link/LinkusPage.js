c8a3e93264e20a4bf469c95b9c09449f2e4ecccd
KshatriyaStarfish <zhaoyanran3@sina.com>
2019-11-24 17:21:05 -0500

    finish PublishFormPage.js

--- screens/link/LinkusPage.js
+++ screens/link/LinkusPage.js
@@ -1,14 +1,21 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, Button, Modal } from "react-native";
 import { SearchBar } from "react-native-elements";
 import PostingCard from "./PostingCard";
+import PublishFormPage from "./PublishFormPage";
 import SearchBar2 from "./SearchBar2.js"
 import { Container, Header, Content } from "native-base";
 
 class LinkusPage extends React.Component {
   state = {
-    search: ""
+    search: "",
+    modalVisible:false
   };
+
+  constructor(props) {
+    super(props)
+  }
+
   updateSearch = search => {
     this.setState({ search });
   };
@@ -34,8 +41,24 @@ class LinkusPage extends React.Component {
         </Header>  */}
         <Header>
           <SearchBar2 />
+          <Button title="post"
+          onPress={() => this.setState({modalVisible:true})}>
+            <Text>post</Text>
+          </Button>
         </Header>
         
+        <Modal
+          animationType="none"
+          transparent={false}
+          visible={this.state.modalVisible}
+          onRequestClose={() => this.setState({modalVisible:false})}
+        >
+          <PublishFormPage />
+          <Button
+            onPress={() => this.setState({modalVisible:false})}
+            title="取消"
+            />
+        </Modal>
        
         <Content padder>
           <PostingCard />
