2c57cad1d0c0debec58115609cb986acb4e916ed
lsywua3 <michaelsiyangliu@gmail.com>
2019-11-25 12:02:08 -0500

    Created a floating action button + the buttons for post action and activate action in the LinkusPage

--- screens/link/LinkusPage.js
+++ screens/link/LinkusPage.js
@@ -2,19 +2,53 @@ import React from "react";
 import { Text, View } from "react-native";
 import { SearchBar } from "react-native-elements";
 import PostingCard from "./PostingCard";
-import SearchBar2 from "./SearchBar2.js"
-import { Container, Header, Content } from "native-base";
+import SearchBar2 from "./SearchBar2.js";
+import { Container, Header, Content, Button, Icon, Fab} from "native-base";
+
+// Trying to solve the font issue:
+// import { Font, AppLoading } from 'expo';
 
 class LinkusPage extends React.Component {
+  
   state = {
-    search: ""
+    search: "",
   };
   updateSearch = search => {
     this.setState({ search });
   };
+  
+  constructor(props) {
+    super(props)
+    this.state = {
+      active: false,
+      /*
+      //Trying to solve the font issue
+      loading: true
+      */
+    };
+  }
+  /*
+  // Trying to solve the font issue
+  async componentWillMount() {
+    await Font.loadAsync({
+      Roboto: require("native-base/Fonts/Roboto.ttf"),
+      Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf")
+    });
+    this.setState({ loading: false });
+  }
+*/
   render() {
     const { search } = this.state;
-
+   /*
+   //Trying to solve the font issue
+    if (this.state.loading) {
+      return (
+        <Root>
+          <AppLoading />
+        </Root>
+      );
+    }
+    */
     return (
       // <View>
       // <SearchBar
@@ -24,7 +58,9 @@ class LinkusPage extends React.Component {
       // />
       // <PostingCard />
       // </View>
+     
       <Container>
+         
         {/* <Header>
           <SearchBar
             placeholder="Type Here..."
@@ -35,13 +71,32 @@ class LinkusPage extends React.Component {
         <Header>
           <SearchBar2 />
         </Header>
-        
-       
+
+
         <Content padder>
           <PostingCard />
           <PostingCard />
           <PostingCard />
+         
         </Content>
+
+        <Fab
+            active={this.state.active}
+            direction="up"
+            containerStyle={{ }}
+            style={{ backgroundColor: '#5067FF' }}
+            position="bottomRight"
+            onPress={() => this.setState({ active: !this.state.active })}>
+            
+            <Icon name="ios-arrow-up"/>
+            <Button onPress={() => alert("Write Something (except you can't write currently :D)")} style={{ backgroundColor: '#3B5998' }}>
+              <Icon name="add" />
+            </Button>
+            <Button onPress={() => alert("Choose a post to activate (except you can't choose currently :D)")} style={{ backgroundColor: '#DD5144' }}>
+            <Icon name="ios-flame" />
+            </Button>
+          </Fab>
+
       </Container>
     );
   }
