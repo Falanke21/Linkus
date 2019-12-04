7fcaccea47278c1188fd008de761c758f8ad47f3
rainyuxuan <yu2001xuan@gmail.com>
2019-12-04 16:07:36 -0500

    change lists

--- screens/setting/SettingPage.js
+++ screens/setting/SettingPage.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import {Text, View, StyleSheet, Image, TouchableOpacity} from 'react-native';
-import {Button, ListItem, Divider} from 'react-native-elements';
+import {Button, ListItem, Divider, Icon} from 'react-native-elements';
 
 export default class SettingPage extends React.Component {
+  static navigationOptions = {
+    title: '设置',
+  };
+
   render() {
     return (
       <View>
@@ -12,41 +16,57 @@ export default class SettingPage extends React.Component {
               key={i}
               title={item.title}
               leftIcon={{name: item.icon}}
-              bottomDivider
+              //bottomDivider
               chevron
             />
           ))}
         </View>
-        <View style={{height:"10%"}}></View>
-        <Divider/>
+        <View style={{height: '2%', backgroundColor: '#e5e5e5'}}></View>
         <View>
           {list2.map((item, i) => (
             <ListItem
               key={i}
               title={item.title}
               leftIcon={{name: item.icon}}
-              bottomDivider
+              //bottomDivider
+              chevron
+            />
+          ))}
+        </View>
+        <View style={{height: '2%', backgroundColor: '#e5e5e5'}}></View>
+        <View>
+          {list3.map((item, i) => (
+            <ListItem
+              key={i}
+              title={item.title}
+              leftIcon={{name: item.icon}}
+              //bottomDivider
               chevron
             />
           ))}
         </View>
         <View
-            style={{padding: 30, width:"100%", alignItems: 'center', justifyContent: 'center'}}
+          style={{
+            padding: 30,
+            width: '100%',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
+          <Button
+            onPress={() => this.props.navigation.navigate('Login')}
+            title="Login"
+            type="outline"
+            buttonStyle={{width: 300}}
+          />
+          <TouchableOpacity
+            onPress={() => {
+              this.props.navigation.navigate('Profile');
+            }}
           >
-            <Button
-              onPress={() => this.props.navigation.navigate('Login')}
-              title="Login"
-              type="outline"
-              buttonStyle={{width: 300}}
-            />
-            <TouchableOpacity
-              onPress={() => {
-                this.props.navigation.navigate('Profile');
-              }}
-            >
-              <Text>Cancel</Text>
-            </TouchableOpacity>
-          </View>
+            <Text>Cancel</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     );
   }
@@ -61,17 +81,13 @@ const list1 = [
     title: '隐私设置',
     icon: 'pets',
   },
-  {
-    title: '账号安全',
-    icon: 'perm-identity',
-  },
+];
+
+const list2 = [
   {
     title: '消息提醒',
     icon: 'account-circle',
   },
-];
-
-const list2 = [
   {
     title: '语言',
     icon: 'rowing',
@@ -79,5 +95,15 @@ const list2 = [
   {
     title: '清理缓存',
     icon: 'pets',
+  },
+];
+const list3 = [
+  {
+    title: '反馈与帮助',
+    icon: 'account-circle',
+  },
+  {
+    title: '关于Linkus',
+    icon: 'account-circle',
   },
 ];
