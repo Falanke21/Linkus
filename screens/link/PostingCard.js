c13b68a76bd3f42b5d42ab13abcc9cd7f4047375
rainyuxuan <yu2001xuan@gmail.com>
2019-12-05 00:38:32 -0500

    redesigned

--- screens/link/PostingCard.js
+++ screens/link/PostingCard.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, {Component} from 'react';
 import {
   Card,
   CardItem,
@@ -8,41 +8,25 @@ import {
   Right,
   Button,
   Icon,
-  Text
-} from "native-base";
+  Text,
+} from 'native-base';
 export default class CardItemButton extends Component {
   render() {
     return (
       <Card>
-        <CardItem header button onPress={() => alert("咕咕咕")}>
+        <CardItem header button onPress={() => alert('咕咕咕')}>
           <Left>
             <Thumbnail
               source={{
                 uri:
-                  "https://interactive-examples.mdn.mozilla.net/media/examples/grapefruit-slice-332-332.jpg"
+                  'https://interactive-examples.mdn.mozilla.net/media/examples/grapefruit-slice-332-332.jpg',
               }}
             />
             <Body>
-              <Text>Frank Hua</Text>
-              <Text note>鸽子</Text>
+              <Text>CSC3xx 找队友</Text>
+              <Text note>我一定不会鸽的</Text>
             </Body>
           </Left>
-        </CardItem>
-        <CardItem button onPress={() => alert("This is Card Body")}>
-          <Body>
-            <Text>CSC3xx 找队友</Text>
-          </Body>
-        </CardItem>
-        <CardItem footer button onPress={() => alert("This is Card Footer")}>
-          <Left>
-            <Text>我一定不会鸽的</Text>
-          </Left>
-          <Right>
-            <Button transparent>
-              <Icon active name="chatbubbles" />
-              <Text>4 Comments</Text>
-            </Button>
-          </Right>
         </CardItem>
       </Card>
     );
