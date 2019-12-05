45921d7600731ec89cebfa098fd7f8ad91b59cc5
rainyuxuan <yu2001xuan@gmail.com>
2019-12-05 00:46:24 -0500

    redesigned

--- screens/link/LinkusPage.js
+++ screens/link/LinkusPage.js
@@ -1,35 +1,32 @@
-import React from "react";
-import { Text, View, Button, Modal } from "react-native";
-import { SearchBar } from "react-native-elements";
-import PostingCard from "./PostingCard";
-import PublishFormPage from "./PublishFormPage";
-import SearchBar2 from "./SearchBar2.js";
-import { Container, Header, Content, Icon, Fab} from "native-base";
-
-
+import React from 'react';
+import {Text, View, Button, Modal} from 'react-native';
+import {SearchBar} from 'react-native-elements';
+import PostingCard from './PostingCard';
+import PublishFormPage from './PublishFormPage';
+import SearchBar2 from './SearchBar2.js';
+import {Container, Header, Content, Icon, Fab} from 'native-base';
 
 class LinkusPage extends React.Component {
-  
   state = {
-    search: "",
-    modalVisible:false
+    search: '',
+    modalVisible: false,
   };
 
   updateSearch = search => {
-    this.setState({ search });
+    this.setState({search});
   };
-  
+
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       active: false,
-     
+      modalVisible: false,
     };
   }
- 
+
   render() {
-    const { search } = this.state;
-   
+    const {search} = this.state;
+
     return (
       // <View>
       // <SearchBar
@@ -39,9 +36,8 @@ class LinkusPage extends React.Component {
       // />
       // <PostingCard />
       // </View>
-     
+
       <Container>
-         
         {/* <Header>
           <SearchBar
             placeholder="Type Here..."
@@ -51,50 +47,67 @@ class LinkusPage extends React.Component {
         </Header>  */}
         <Header>
           <SearchBar2 />
-          <Button title="post"
-          onPress={() => this.setState({modalVisible:true})}>
+          {/*<Button
+            title="post"
+            onPress={() => this.setState({modalVisible: true})}
+          >
             <Text>post</Text>
-          </Button>
+          </Button>*/}
         </Header>
-        
+
         <Modal
           animationType="none"
           transparent={false}
           visible={this.state.modalVisible}
-          onRequestClose={() => this.setState({modalVisible:false})}
+          onRequestClose={() => this.setState({modalVisible: false})}
         >
           <PublishFormPage />
           <Button
-            onPress={() => this.setState({modalVisible:false})}
+            onPress={() => this.setState({modalVisible: false})}
             title="取消"
-            />
+          />
         </Modal>
-       
+
         <Content padder>
           <PostingCard />
           <PostingCard />
           <PostingCard />
-         
+          <PostingCard />
+          <PostingCard />
+          <PostingCard />
+          <PostingCard />
         </Content>
-        
-        <Fab
-        active={this.state.active}
-        direction="up"
-        containerStyle={{ }}
-        style={{ backgroundColor: '#5067FF' }}
-        position="bottomRight"
-        onPress={() => this.setState({ active: !this.state.active })}>
-        
-        <Icon name="ios-arrow-up"/>
-        <Button onPress={() => this.setState({modalVisible:true})} style={{ backgroundColor: '#3B5998' }}>
-          <Icon name="add" />
-        </Button>
-        <Button onPress={() => alert("Choose a post to activate (except you can't choose currently :D)")} style={{ backgroundColor: '#DD5144' }}>
-        <Icon name="ios-flame" />
-        </Button>
-      </Fab>
-        
 
+        <Fab
+          //active={this.state.active}
+          active={true}
+          direction="up"
+          containerStyle={{}}
+          //style={{backgroundColor: '#5067FF'}}
+          style={{backgroundColor: '#062958'}}
+          position="bottomRight"
+          //onPress={() => this.setState({active: !this.state.active})}
+          onPress={() => this.setState({modalVisible: true})}
+        >
+          <Icon name="add" />
+          {/*<Icon name="ios-arrow-up" />
+          <Button
+            onPress={() => this.setState({modalVisible: true})}
+            style={{backgroundColor: '#3B5998'}}
+          >
+            <Icon name="add" />
+          </Button>*/}
+          <Button
+            onPress={() =>
+              alert(
+                "Choose a post to activate (except you can't choose currently :D)"
+              )
+            }
+            style={{backgroundColor: '#DD5144'}}
+          >
+            <Icon name="ios-flame" />
+          </Button>
+        </Fab>
       </Container>
     );
   }
