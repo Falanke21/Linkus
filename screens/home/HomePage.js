5af64f6be6e8055fe5c05d8a1d3f67d3075f0f8a
kexinlin <kexin.lin@mail.utoronto.ca>
2020-02-18 15:53:47 -0500

    finish the banner on the home page

--- screens/home/HomePage.js
+++ screens/home/HomePage.js
@@ -1,18 +1,40 @@
-import React from 'react';
-import {Text, View, StatusBar} from 'react-native';
+import React from "react";
+import { Text, View, StatusBar } from "react-native";
+import Carousel from "react-native-banner-carousel";
+import { StyleSheet, Image, Dimensions } from "react-native";
+
+const BannerWidth = Dimensions.get("window").width;
+const BannerHeight = 260;
+
+const images = [
+  "https://images.dailyhive.com/20190401115942/shutterstock_1252186957.jpg",
+  "https://images.dailyhive.com/20180807073521/shutterstock_450746794.jpg",
+  "https://www.daniels.utoronto.ca/sites/default/files/styles/standard_image_3_col/public/pages/uoft_uc-east_300dpi.jpg"
+];
 
 export default class HomePage extends React.Component {
   static navigationOptions = {
-    title: '首页',
+    title: "首页"
     //header: null,
   };
+
+  renderPage(image, index) {
+    return (
+      <View key={index}>
+        <Image
+          style={{ width: BannerWidth, height: BannerHeight }}
+          source={{ uri: image }}
+        />
+      </View>
+    );
+  }
+
   render() {
     return (
       <View
         style={{
           flex: 1,
-          justifyContent: 'center',
-          alignItems: 'center',
+          justifyContent: "top"
         }}
       >
         <StatusBar
@@ -20,8 +42,19 @@ export default class HomePage extends React.Component {
           translucent={true}
           hidden={true}
           animated={true}
+          justifyContent="center"
+          flex={1}
         />
-        <Text>Home page</Text>
+
+        <Carousel
+          autoplay
+          autoplayTimeout={5000}
+          loop
+          index={0}
+          pageSize={BannerWidth}
+        >
+          {images.map((image, index) => this.renderPage(image, index))}
+        </Carousel>
       </View>
     );
   }
