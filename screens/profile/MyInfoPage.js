63d16696b792ea19ea6d61be23ef29cedf1b96a3
rainyuxuan <yu2001xuan@gmail.com>
2020-03-12 02:05:12 -0400

    debug了MyInfo

--- screens/profile/MyInfoPage.js
+++ screens/profile/MyInfoPage.js
@@ -5,8 +5,9 @@ import {
   TextInput,
   StyleSheet,
   Modal,
-  FlatList,
+  Picker,
   ScrollView,
+  Alert,
 } from 'react-native';
 import {
   Button,
@@ -39,6 +40,7 @@ export default class MyLinkPage extends React.Component {
     changingType: 'DNE',
     changingTitle: 'DNE',
     changingContent: 'DNE',
+    changingKey: 'username',
     basicInfo: {
       username: {
         title: '用户名',
@@ -56,21 +58,19 @@ export default class MyLinkPage extends React.Component {
         title: '校区',
         content: user.campus,
         type: 'campus',
-        canModify: false,
+        canModify: true,
       },
       courses: {
         title: '课程',
         content: user.courses,
         type: 'courses',
         canModify: true,
-        selection_type: 'select',
       },
       description: {
-        title: '签名',
+        title: '简介',
         content: user.description,
         type: 'description',
         canModify: true,
-        selection_type: 'text',
       },
     },
     contacts: user.contacts,
@@ -86,14 +86,29 @@ export default class MyLinkPage extends React.Component {
     console.log('will unmount');
   }
 
-  pressList(item) {
-    console.log('over lay show');
+  pressList(item, key) {
+    console.log('into pressList: ' + item.type);
     this.setState({
       isVisible: item.canModify,
       changingType: item.type,
       changingTitle: item.title,
       changingContent: item.content,
+      changingKey: key,
     });
+    if (!item.canModify) {
+      Alert.alert(
+        '',
+        '此项不可修改！',
+        [
+          {
+            text: 'Cancel',
+            onPress: () => console.log('Cancel Pressed'),
+            style: 'cancel',
+          },
+        ],
+        {cancelable: true}
+      );
+    }
   }
 
   TextInputHandler(content) {
@@ -108,7 +123,11 @@ export default class MyLinkPage extends React.Component {
 
     let _type = this.state.changingType;
 
-    this.state.basicInfo[_type].content = this.state.changingContent;
+    if (_type == 'courses') {
+      this.state.basicInfo[_type].content = ["修改了1", "修改了2"]
+    } else {
+      this.state.basicInfo[_type].content = this.state.changingContent;
+    }
 
     console.log('finish modify');
     console.log(
@@ -120,31 +139,108 @@ export default class MyLinkPage extends React.Component {
   }
 
   showOverlay(item) {
-    console.log('trying to show');
-    //modify_part = <View></View>;
-    console.log(item);
+    console.log('Into showOverlay: ' + item.type);
+    //modifyingPart = <View></View>;
+    console.log(item.type);
     if (item) {
-      if (item.selection_type == 'select') {
-        //modify_part to scroll
-      } else {
-        modify_part = (
-          <TextInput
-            onChangeText={content => this.TextInputHandler(content)}
-            value={this.state.changingContent}
-            autoCapitalize="none"
-            autoCorrect={false}
-            returnKeyType="go"
+      if (item.type == 'username') {
+        modifyingPart = (
+          <View
             style={{
-              borderColor: '#062958',
-              borderWidth: 2,
-              borderRadius: 25,
-              textAlign: 'center',
-              height: 50,
-              width: '90%',
-              alignContent: 'flex-start',
-              fontSize: 18,
+              height: '50%',
+              width: '100%',
+              justifyContent: 'center',
+              alignItems: 'center',
             }}
-          />
+          >
+            <TextInput
+              onChangeText={content => this.TextInputHandler(content)}
+              value={this.state.changingContent}
+              autoCapitalize="none"
+              autoCorrect={false}
+              returnKeyType="go"
+              style={{
+                borderColor: '#062958',
+                borderWidth: 2,
+                borderRadius: 25,
+                textAlign: 'center',
+                height: 50,
+                width: '90%',
+                alignContent: 'flex-start',
+                fontSize: 18,
+              }}
+            />
+          </View>
+        );
+      } else if (item.type == 'campus') {
+        modifyingPart = (
+          <View
+            style={{
+              height: '50%',
+              width: '100%',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+          >
+            <Picker
+              selectedValue={this.state.changingContent}
+              style={{height: 50, width: '60%'}}
+              onValueChange={(itemValue, itemIndex) =>
+                this.setState({changingContent: itemValue})
+              }
+              mode="dropdown"
+            >
+              <Picker.Item label="UTSG" value="UTSG" />
+              <Picker.Item label="UTM" value="UTM" />
+              <Picker.Item label="UTSC" value="UTSC" />
+            </Picker>
+          </View>
+        );
+      } else if (item.type == 'courses') {
+        modifyingPart = (
+          <View
+            style={{
+              height: '50%',
+              width: '100%',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+          >
+            <Text>Developing！</Text>
+          </View>
+        );
+      } else if (item.type == 'description') {
+        modifyingPart = (
+          <View
+            style={{
+              height: '50%',
+              width: '100%',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+          >
+            <TextInput
+              onChangeText={content => this.TextInputHandler(content)}
+              value={this.state.changingContent}
+              placeholder="写点什么吧！"
+              autoCapitalize="none"
+              autoCorrect={false}
+              returnKeyType="go"
+              multiline={true}
+              maxLength={20}
+              style={{
+                //borderColor: '#062958',
+                //borderWidth: 2,
+                //borderRadius: 25,
+                textAlign: 'left',
+                height: 100,
+                width: '70%',
+                alignContent: 'flex-start',
+                fontSize: 16,
+              }}
+            />
+            <Text style={{color: 'gray'}}>最多20字！</Text>
+          </View>
         );
       }
     }
@@ -153,24 +249,17 @@ export default class MyLinkPage extends React.Component {
       <Overlay
         isVisible={this.state.isVisible}
         overlayBackgroundColor="white"
-        windowBackgroundColor="none"
+        windowBackgroundColor="rgba(000, 000, 000, .3)"
         onBackdropPress={() => this.setState({isVisible: false})}
+        animationType="fade"
         width="80%"
         height="40%"
       >
         <Text style={{alignSelf: 'center', fontSize: 20, padding: 15}}>
           {this.state.changingTitle}
         </Text>
-        <View
-          style={{
-            height: '50%',
-            width: '100%',
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}
-        >
-          {modify_part}
-        </View>
+
+        {modifyingPart}
 
         <View
           style={{
@@ -221,14 +310,14 @@ export default class MyLinkPage extends React.Component {
           <Divider />
           {Object.keys(this.state.basicInfo).map((item, i) => (
             <View style={{backgroundColor: 'black'}}>
-              {this.showOverlay(this.state.basicInfo[item])}
               <ListItem
                 key={i}
                 title={this.state.basicInfo[item].title}
                 titleStyle={{fontSize: 16}}
                 rightTitle={this.state.basicInfo[item].content}
+                rightTitleProps={{numberOfLines: 1}}
                 onPress={() => {
-                  this.pressList(this.state.basicInfo[item]);
+                  this.pressList(this.state.basicInfo[item], item);
                 }}
                 containerStyle={{height: 50}}
                 //bottomDivider
@@ -236,6 +325,7 @@ export default class MyLinkPage extends React.Component {
               />
             </View>
           ))}
+          {this.showOverlay(this.state.basicInfo[this.state.changingKey])}
         </View>
         <View style={{width: '100%', color: '#FF1'}}>
           <Text style={{fontSize: 16, padding: 15, fontWeight: 'bold'}}>
@@ -248,7 +338,7 @@ export default class MyLinkPage extends React.Component {
               title={item}
               titleStyle={{fontSize: 16}}
               rightTitle={this.state.contacts[item]}
-              // rightTitleNumberOfLines={1}
+              rightTitleProps={{numberOfLines: 1}}
               onPress={() =>
                 this.pressList('联系', item, this.state.contacts[item])
               }
